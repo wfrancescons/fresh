dc6400a2ca1769e466393b86b39ae173a336429b
Marvin Hagemeister <marvin@deno.com>
2025-08-19 17:51:46 +0200

    WIP

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,12 +1,51 @@
-import { defineConfig } from "vite";
+import { defineConfig, type Plugin } from "vite";
 import { fresh } from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
 import inspect from "vite-plugin-inspect";
+import { copy } from "@std/fs";
+import * as path from "@std/path";
 
 export default defineConfig({
   plugins: [
     fresh(),
     tailwindcss(),
     inspect(),
+    copyDocs(),
   ],
 });
+
+function copyDocs(): Plugin {
+  let serverOutDir = "";
+
+  return {
+    name: "fresh:copy-docs",
+    configResolved(config) {
+      serverOutDir = config.environments.ssr.build.outDir;
+    },
+    async writeBundle() {
+      const branches = ["canary", "latest"];
+
+      for (const branch of branches) {
+        const source = path.join(import.meta.dirname!, "..", "docs", branch);
+        const target = path.join(serverOutDir, "docs", branch);
+
+        try {
+          await Deno.remove(target, { recursive: true });
+        } catch (err) {
+          if (!(err instanceof Deno.errors.NotFound)) {
+            throw err;
+          }
+        }
+
+        try {
+          await Deno.mkdir(path.dirname(target), { recursive: true });
+        } catch {
+          // Ignore
+        }
+
+        // await new Promise((r) => setTimeout(r, 2220000));
+        await copy(source, target);
+      }
+    },
+  };
+}
